54e1db78a27ec9efe3986a207c2b7f7c4dc94592
Your Name <you@example.com>
2021-10-23 23:34:35 +0200

    New design

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get('/route', (req, res)=>{
             });*/
 
 
-            console.log(dvbData.substring(dvbData.indexOf('<h1 class="heading style11">Verbindungsauskunft</h1>'), dvbData.indexOf('<h2 class="heading style12">Fahrtdetails</h2>')))
+            //console.log(dvbData.substring(dvbData.indexOf('<h1 class="heading style11">Verbindungsauskunft</h1>'), dvbData.indexOf('<h2 class="heading style12">Fahrtdetails</h2>')))
 
 
             res.render('index',{
@@ -66,74 +66,9 @@ app.get('/route', (req, res)=>{
 
 })
 
-app.get('/tramMap', (req, res) =>{
-    var title = "Dynamobility"
-    res.render('trams', { title })
-})
-
-app.get('/test', (req, res) => {
-    if (!req.query.radius) {
-        return res.send({
-            error: 'You must provide a radius range!'
-        })
-    }
-    
-    nextbike(req.query.radius, (error, nextBikeData) => {
-            if (error) {
-                return res.send({ error })
-            }
-        
-            const mobiStationsSource = nextBikeData.countries[0].cities[0].places
-            var mobiStations = []
-            
-            //console.log(mobiStations)
-            mobiStationsSource.forEach(element => {
-                mobiStations.push({ name: element.name, lat: element.lat, lng: element.lng,  bikes: element.bikes_available_to_rent})
-            });
-
-            res.render('index',{
-                mobiStations: mobiStations
-            
-            })
-        })
-    
-})
 
 app.post('/result', (req, res) => {
     const { userinfo } = req.body;
-    
-})
-
-
-app.get('/dvb', (req, res) => {
-    /*if (!req.query.radius) {
-        return res.send({
-            error: 'You must provide a radius range!'
-        })
-    }*/
-    
-    dvb( (error, dvbData) => {
-            if (error) {
-                return res.send({ error })
-            }
-        
-            /*const mobiStationsSource = nextBikeData.countries[0].cities[0].places
-            var mobiStations = []
-            
-            //console.log(mobiStations)
-            mobiStationsSource.forEach(element => {
-                mobiStations.push({ name: element.name, lat: element.lat, lng: element.lng,  bikes: element.bikes_available_to_rent})
-            });*/
-
-
-            console.log(dvbData.substring(dvbData.indexOf('<h1 class="heading style11">Verbindungsauskunft</h1>'), dvbData.indexOf('<h2 class="heading style12">Fahrtdetails</h2>')))
-
-
-            res.render('routeplan',{
-                dvbData: dvbData.substring(dvbData.indexOf('<h1 class="heading style11">Verbindungsauskunft</h1>'), dvbData.indexOf('<h2 class="heading style12">Fahrtdetails</h2>'))
-            
-            })
-        })
     
 })
 
